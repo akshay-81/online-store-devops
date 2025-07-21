c0bcf6205998d6c1459c227a45246ea7091aca22
Akshay <akshaykrishnan8125@gmail.com>
2025-07-21 20:04:43 +0530

    Update index.js and add node_modules to .gitignore

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.get('/test-db', async (_, res) => {
   try {
     const result = await pool.query('SELECT NOW()');
     res.json({ success: true, time: result.rows[0].now });
+    console.log("HI");
   } catch (err) {
     console.error('DB error:', err);
     res.status(500).json({ success: false, message: 'Database connection failed' });
